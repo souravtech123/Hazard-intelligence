1114eef929bfb36f1ef5ca45f41ec1ea2af07c25
souravtech123 <souravsuman846@gmail.com>
2026-09-06 17:44:15 +0530

    Production

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const env = {
   PORT: Number(process.env.PORT) || 5000,
 
-  DATABASE_URL: process.env.DATABASE_URL || "postgresql://sourav:sourav2026@localhost:5432/sih_db",
+  DATABASE_URL: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_aiPRf0JOoZt6@ep-mute-sea-ayx1v7lc-pooler.c-5.us-east-2.aws.neon.tech/hazarddb?sslmode=require&channel_binding=require",
 
   NODE_ENV: process.env.NODE_ENV || "development",
 };
